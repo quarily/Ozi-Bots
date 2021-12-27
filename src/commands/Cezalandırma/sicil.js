f0fc2e22d66783cf5c6db6fe4d2d3688b8b19853
Jaylen Ozi <92666466+JaylenOzi@users.noreply.github.com>
2021-12-28 01:45:31 +0300

    Update sicil.js

--- src/commands/Cezalandırma/sicil.js
+++ src/commands/Cezalandırma/sicil.js
@@ -13,7 +13,7 @@ module.exports = {
     const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
     let data = await penals.find({ guildID: message.guild.id, userID: member.user.id, }).sort({ date: -1 });
     if (data.length === 0) return message.channel.send(`${green} ${member.toString()} üyesinin sicili temiz!`).then(x=>x.delete({timeout:5000}))
-    data = data.map((x) => `#${x.id} ${x.active ? green : red} **[${x.type}]** ${moment(x.date).format("LLL")} tarihinde, <@${x.staff}> tarafından, \`${x.reason}\` nedeniyle, ${x.type.toLowerCase().replace("-", " ")} cezası almış.`).join("\n");
+    data = data.map((x) => `#${x.id} ${x.active ? green : red} **[${x.type}]** ${moment(x.date).format("LLL")} tarihinde, <@${x.staff}> tarafından, \`${x.reason}\` nedeniyle, ${x.type.toLowerCase().replace("-", " ")} cezası almış.\n─────────────────`).join("\n");
     for (var i = 0; i < Math.floor(data.length / 2000); i++) {
       message.channel.send(embed.setDescription(data.slice(0, 2000)));
       data = data.slice(2000);
