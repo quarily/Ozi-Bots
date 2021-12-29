cf0d32ab3b66ae09eb741ea0b9dd0a5e57d3ac56
Jaylen Ozi <92666466+JaylenOzi@users.noreply.github.com>
2021-12-29 20:16:17 +0300

    Star At Abim

--- src/schemas/bannedTag.js
+++ src/schemas/bannedTag.js
@@ -2,7 +2,7 @@ const { Schema, model } = require("mongoose");
 
 const schema = Schema({
 	guildID: { type: String, default: "" },
-	tags: { type: Array, default: [] }
+	taglar: { type: Array, default: [] }
 });
 
 module.exports = model("bannedTag", schema);
