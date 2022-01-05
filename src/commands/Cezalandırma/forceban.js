8a7a0331c28ebba754156ca5da99c12fa2b335fb
Jaylen Ozi <92666466+JaylenOzi@users.noreply.github.com>
2022-01-05 17:52:59 +0300

    Star Atmayı Unutmayınız <3

--- src/commands/Cezalandırma/forceban.js
+++ src/commands/Cezalandırma/forceban.js
@@ -3,7 +3,7 @@ moment.locale("tr");
 const forceBans = require("../../schemas/forceBans");
 const conf = require("../../configs/sunucuayar.json")
 const settings = require("../../configs/settings.json")
-const { red, green } = require("../../configs/emojis.json")
+const { red, green, Cezaa, Revuu, kirmiziok } = require("../../configs/emojis.json")
 module.exports = {
   conf: {
     aliases: ["forceban"],
@@ -29,32 +29,28 @@ module.exports = {
     return }
     const reason = args.slice(1).join(" ") || "Belirtilmedi!";
     const member = message.guild.members.cache.get(user.id);
-    if (message.member.roles.highest.position <= member.roles.highest.position) return message.channel.send("Kendinle aynı yetkide ya da daha yetkili olan birini banlayamazsın!").then(x=>x.delete({timeout:5000}))
+    if (message.guild.members.cache.has(user.id) && message.member.roles.highest.position <= message.guild.members.cache.get(user.id).roles.highest.position) return message.channel.send("Kendinle aynı yetkide ya da daha yetkili olan birini banlayamazsın!").then(x=>x.delete({timeout:5000}))
     if (member && !member.bannable) {
     message.react(red)
-    message.channel.send( "Bu üyeyi banlayamıyorum!").then(x=>x.delete({timeout:5000})) }
+    message.channel.send("Bu üyeyi banlayamıyorum!").then(x=>x.delete({timeout:5000})) }
+
+    if (settings.dmMessages) user.send(`**${message.guild.name}** sunucusundan, **${message.author.tag}** tarafından, **${reason}** sebebiyle **kalıcı olarak** banlandınız!`).catch(() => {});
+
     message.guild.members.ban(user.id, { reason }).catch(() => {});
     await new forceBans({ guildID: message.guild.id, userID: user.id, staff: message.author.id }).save();
     const penal = await client.penalize(message.guild.id, user.id, "FORCE-BAN", true, message.author.id, reason);
-    const gifs = ["https://media1.tenor.com/images/ed33599ac8db8867ee23bae29b20b0ec/tenor.gif?itemid=14760307", "https://media.giphy.com/media/fe4dDMD2cAU5RfEaCU/giphy.gif", "https://media1.tenor.com/images/4732faf454006e370fa9ec6e53dbf040/tenor.gif?itemid=14678194"];
+
     message.lineReply(`${green} ${member ? member.toString() : user.username} üyesi, ${message.author} tarafından, \`${reason}\` nedeniyle **kalıcı olarak** banlandı! \`(Ceza ID: #${penal.id})\``).then(x=>x.delete({timeout:50000}))
     message.react(green)
-    if (settings.dmMessages) user.send(`**${message.guild.name}** sunucusundan, **${message.author.tag}** tarafından, **${reason}** sebebiyle **kalıcı olarak** banlandınız!`).catch(() => {});
 
     const log = embed
-      .setAuthor(user.username, user.avatarURL({ dynamic: true, size: 2048 }))
-      .setColor("#2f3136")
       .setDescription(`
-${member ? member.toString() : user.username} üyesi **kalıcı olarak** banlandı!
-
-Ceza ID: \`#${penal.id}\`
-Banlanan Üye: ${member ? member.toString() : ""} \`(${user.username.replace(/\`/g, "")} - ${user.id})\`
-Banlayan Yetkili: ${message.author} \`(${message.author.username.replace(/\`/g, "")} - ${message.author.id})\`
-Ban Tarihi: \`${moment(Date.now()).format("LLL")}\`
-Ban Sebebi: \`${reason}\`
+${Cezaa} Banlanan Üye: **${member ? member.toString() : user.username}** \`${user.id}\`
+${Revuu} Banlayan Yetkili: ${message.author} \`${message.author.id}\`
+${kirmiziok} Ban Sebebi: \`${reason}\`
       `)
-      .setImage(gifs.random())
-    message.guild.channels.cache.get(conf.banLogChannel).wsend(log);
+      .setFooter(`${moment(Date.now()).format("LLL")}`)
+      message.guild.channels.cache.get(conf.banLogChannel).send(log);
   },
 };
 
